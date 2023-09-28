fac2aa54115c0e55cd814c037ee984df9fb928b7
Favixx <bogdandom52@gmail.com>
2023-09-28 14:58:24 +0300

    init fixes

---
+++ client/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,jsx,ts,tsx, mjs}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "480px",
+      },
+      fontFamily: {
+        inter: ["Inter var", "sans-serif"],
+      },
+      boxShadow: {
+        card: "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)",
+        cardhover:
+          "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
