2007ce82378d79e9315e0f6c010bfe79ff5fc7ea
Favixx <bogdandom52@gmail.com>
2023-09-28 15:12:34 +0300

    fix: Added container, and main tag

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,10 +9,12 @@ const App: FC = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path={"/"} element={<Home />} />
-        <Route path={"/createpost"} element={<CreatePost />} />
-      </Routes>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh - 73px)]">
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/createpost"} element={<CreatePost />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
