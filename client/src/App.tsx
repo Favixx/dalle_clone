797dc625794438e3e57ae9d872eed9e5213b54fb
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-09-28 12:51:04 +0300

    setup

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import {FC} from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+// import { logo } from './assets/logo.svg';
+import { Home, CreatePost } from './pages';
+import Header from './components/Header';
+
+const App: FC = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path={'/'} element={<Home/>}/>
+        <Route path={'/createpost'} element={<CreatePost/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/CreatePost.tsx
@@ -0,0 +1,9 @@
+import {FC} from 'react'
+
+const CreatePost: FC = () => {
+  return (
+    <div>CreatePost</div>
+  )
+}
+
+export default CreatePost
