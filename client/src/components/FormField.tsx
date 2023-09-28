2f4b7bfd3e3dee465f897e685b49fbd8fb9c70c5
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-09-28 15:39:25 +0300

    FormField component

---
+++ client/src/components/FormField.tsx
@@ -0,0 +1,57 @@
+import {FC} from 'react'
+
+interface IFormField {
+    labelName: string
+    type: 'text' | 'password'
+    name: string
+    placeholder: string
+    value: string
+    handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+    isSurpriseMe?: boolean
+    handleSurpriseMe?: () => void
+}
+
+const FormField: FC<IFormField> = ({
+    labelName,
+    type,
+    name,
+    placeholder,
+    value,
+    handleChange,
+    isSurpriseMe,
+    handleSurpriseMe,
+}) => {
+  return (
+    <div>
+        <div className='flex items-center gap-2 mb-2'>
+            <label 
+                htmlFor={name}
+                className='block text-sm font-medium text-gray-900'
+            >
+                {labelName}
+            </label>
+            {isSurpriseMe && (
+                <button
+                    type='button'
+                    onClick={handleSurpriseMe}
+                    className='font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black'
+                >
+                    Surprise me
+                </button>
+            )}
+        </div>
+        <input
+            type={type}
+            id={name}
+            name={name}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+            placeholder={placeholder}
+            value={value}
+            onChange={handleChange}
+            required
+        />
+    </div>
+  )
+}
+
+export default FormField
--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -1,10 +1,57 @@
-import {FC} from 'react'
+import {FC, useState, ChangeEvent} from 'react'
+import {FormField, Loader} from '../components'
+
+
+
+interface FormState {
+  [key: string]: string;
+}
 
 const CreatePost: FC = () => {
+
+  const [form, setForm] = useState<FormState>({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='px-8 py-4 h-screen'>
+      <div>
+        <h1 className='font-bold text-[#222328] text-3xl'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-sm'>Generate an imaginative image through DALL-E AI and share it with the community</p>
+      </div>
+
+      <form>
+        <FormField
+          labelName="Your Name"
+          type="text"
+          name="name"
+          placeholder="Ex., john doe"
+          value={form.name}
+          handleChange={handleChange}
+        />
+
+        <FormField
+          labelName="Prompt"
+          type="text"
+          name="prompt"
+          placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+          value={form.prompt}
+          handleChange={handleChange}
+          isSurpriseMe
+          handleSurpriseMe={handleSurpriseMe}
+        />
+      </form>
+
+    </div>
   )
 }
 
